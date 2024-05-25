4f83d3b3613f0f63b9ae5e381b06f3adb7e1957d
Nathan <nathan_byrne96@hotmail.co.uk>
2024-05-25 16:09:02 +0100

    Add course UI

---
+++ src/app/course/UnitItem.tsx
@@ -0,0 +1,62 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+import { useState } from "react";
+
+const UnitItem = () => {
+  const [showLessons, setShowLessons] = useState(false);
+
+  return (
+    <Card>
+      <CardHeader>
+        <div className="flex flex-row justify-between">
+          <CardTitle className="text-xl">Some unit title</CardTitle>
+          <Button variant={"ghost"}>Add lesson</Button>
+        </div>
+        <CardDescription className="mt-2">Some description</CardDescription>
+        <div className="flex flex-row">
+          <div
+            className="text-sm hover:underline cursor-pointer flex"
+            onClick={() => {
+              setShowLessons(!showLessons);
+            }}
+          >
+            Show lessons
+          </div>
+        </div>
+      </CardHeader>
+      {showLessons && (
+        <CardContent>
+          <div className="w-full h-[1px] bg-stone-200"></div>
+          <div className="flex flex-col gap-2 mt-6">
+            <UnitLessonItem />
+            <UnitLessonItem />
+            <UnitLessonItem />
+          </div>
+        </CardContent>
+      )}
+    </Card>
+  );
+};
+
+const UnitLessonItem = () => {
+  return (
+    <div className="flex flex-row gap-6 hover:bg-stone-50 p-2 rounded-md">
+      <Link href={"/lesson"}>
+        <h2 className="font-medium text-stone-700 hover:underline">
+          Some lesson title
+        </h2>
+      </Link>
+      <p className="text-stone-500">Some lesson description goes here</p>
+    </div>
+  );
+};
+
+export default UnitItem;
--- src/app/course/page.tsx
+++ src/app/course/page.tsx
@@ -1,5 +1,20 @@
-const Course = () => {
-    return (<h1>Course</h1>)
-}
+import { Button } from "@/components/ui/button";
+import UnitItem from "./UnitItem";
 
-export default Course
+const Course = () => {
+  return (
+    <div className="">
+      <h1 className="text-2xl font-medium text-stone-700">Some course title</h1>
+      <div className="flex flex-row w-full justify-end">
+        <Button>+ Add unit</Button>
+      </div>
+      <div className="flex flex-col mt-8 gap-4">
+        <UnitItem />
+        <UnitItem />
+        <UnitItem />
+      </div>
+    </div>
+  );
+};
+
+export default Course;
