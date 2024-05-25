a8b3add0beef3f360cf84b595398ba625b7acb7a
Nathan <nathan_byrne96@hotmail.co.uk>
2024-05-25 02:09:48 +0100

    Add shadcn. Add shadcn components to navigation bar.

--- src/components/navigationBar.tsx
+++ src/components/navigationBar.tsx
@@ -1,13 +1,17 @@
 import Link from "next/link"
+import { Button } from "@/components/ui/button"
+import { Input } from "./ui/input"
 
 const NavigationBar = () => {
-    return (<div className="w-full bg-stone-800 py-6 px-10 flex flex-row justify-between items-center">
+    return (<div className="w-full bg-stone-100 py-6 px-10 flex flex-row justify-between items-center">
         <div className="flex flex-row gap-10 items-center">
             <Link href={"/"}><h1 className="text-2xl">OpenCourse</h1></Link>
             <div className="flex flex-row gap-4">
                 <Link href={"/dashboard"}>Dashboard</Link>
                 <Link href={"/courses"}>Courses</Link>
             </div>
+            <Input type="text" placeholder="Search" />
+            <Button>+ Create course</Button>
         </div>
         <div className="h-10 w-10 rounded-full bg-pink-200"></div>
     </div>)
